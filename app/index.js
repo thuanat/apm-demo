c80d2458074c4c0a1a60736ab9e6885798988fe0
Thuan Le Van <thuanlv1@officevnpay.com>
2026-01-09 14:21:09 +0700

    loki lan 2

--- app/index.js
+++ app/index.js
@@ -1,26 +1,23 @@
-// app/index.js
-const { logs, SeverityNumber } = require('@opentelemetry/api-logs');
 const express = require('express');
-const logger = require('pino')(); // Pino sẽ được OTel tự động hook
+const logger = require('pino')();
 
+// Demo App
 const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => {
-  logger.info("Yêu cầu tại trang chủ");
-  res.send('Hệ thống APM đang hoạt động!');
+  logger.info("Yêu cầu tại trang chủ"); // Log này sẽ có trace_id tự động
+  res.send('Hệ thống APM đã thông suốt!');
 });
 
 app.get('/error', (req, res) => {
-  // Ghi log lỗi có cấu trúc
   logger.error({ 
-    event: "database_error",
-    db_name: "main_db"
+    error_type: "database",
+    detail: "Connection timeout" 
   }, "Lỗi kết nối Database giả lập");
-  
   res.status(500).send('Lỗi hệ thống');
 });
 
 app.listen(port, () => {
-  console.log(`App demo đang lắng nghe tại http://localhost:${port}`);
+  logger.info(`App demo đang lắng nghe tại http://localhost:${port}`);
 });
